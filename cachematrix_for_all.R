fcd7524be60057b4e664c6f53655fbaf0357bb80
ANUPRAKASH MOOTHEDATH <anuprakashm@gmail.com>
2015-05-13 23:51:08 +0530

    Create cachematrix_for_all.R

---
+++ cachematrix_for_all.R
@@ -0,0 +1,75 @@
+makeCacheMatrix <- function(mat = matrix()) {
+  invmat <- NULL
+  det <- NULL
+  rowcol <- NULL
+  
+  setmat <- function(temp) {
+    mat <<- temp
+    invmat <<- NULL
+    det <<- NULL
+    rowcol <<- NULL
+  }
+  
+  getmat <- function() mat
+  
+  setrowcol <- function(temp) rowcol <<- temp
+  
+  getrowcol <- function() rowcol
+  
+  setdet <- function(temp) det <<- temp
+  
+  getdet <- function() det
+  
+  setinv <- function(inverse) invmat <<- inverse
+  
+  getinv <- function() invmat
+  
+  list(setrowcol = setrowcol, getrowcol = getrowcol ,setdet= setdet, getdet=getdet, setmat = setmat, getmat = getmat, setinv = setinv, getinv = getinv)
+} 
+
+
+cacheSolve <- function(mat) {
+  
+  detmat <- mat$getdet()
+  if (!is.null(detmat)) {
+    if (detmat == 0) {
+      message("Cached message: The matrix is non invertible since determinant is zero")
+      return(numeric())
+    }
+  }
+  
+  rowcolmat <- mat$getrowcol()
+  if (!is.null(rowcolmat)) {
+    if (rowcolmat == FALSE) {
+      message("Cached message: The matrix is non invertible since row numbers not equal to column numbers")
+      return(numeric())
+    }
+  }
+  
+  invmat <- mat$getinv()
+  if (!is.null(invmat)) {
+    message("Getting matrix inverse from cache")
+    return(invmat)
+  }
+  
+  
+  matdata <- mat$getmat()
+  
+  if (nrow(matdata) != ncol(matdata)) {
+    message("The matrix is non invertible since row numbers not equal to column numbers")
+    mat$setrowcol(FALSE)
+    return(numeric())
+  }
+  mat$setrowcol(TRUE)
+  
+  detdata <- det(matdata)
+  mat$setdet(detdata)
+  if (detdata == 0) {
+    message("The matrix is non invertible since determinant is zero")
+    return(numeric())
+  }
+  
+  invmat <- solve(matdata)
+  mat$setinv(invmat)
+  return(invmat)
+}
