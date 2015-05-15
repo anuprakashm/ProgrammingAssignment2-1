8243af1e865c72fba42ec7a0c0ef6d32e676f472
ANUPRAKASH MOOTHEDATH <anuprakashm@gmail.com>
2015-05-15 07:36:11 +0530

    Update cachematrix_advanced.R

--- cachematrix_advanced.R
+++ cachematrix_advanced.R
@@ -28,32 +28,32 @@
 ## 8 ) getinv() -> This function will get the inverse of the matrix
 
 makeCacheMatrix <- function(mat = matrix()) {
-  invmat <- NULL
-  det <- NULL
-  rowcol <- NULL
-  
-  setmat <- function(temp) {
-    mat <<- temp
-    invmat <<- NULL
-    det <<- NULL
-    rowcol <<- NULL
-  }
-  
-  getmat <- function() mat
-  
-  setrowcol <- function(temp) rowcol <<- temp
-  
-  getrowcol <- function() rowcol
-  
-  setdet <- function(temp) det <<- temp
-  
-  getdet <- function() det
-  
-  setinv <- function(inverse) invmat <<- inverse
-  
-  getinv <- function() invmat
-  
-  list(setrowcol = setrowcol, getrowcol = getrowcol ,setdet= setdet, getdet=getdet, setmat = setmat, getmat = getmat, setinv = setinv, getinv = getinv)
+        invmat <- NULL
+        det <- NULL
+        rowcol <- NULL
+        
+        setmat <- function(temp) {
+                mat <<- temp
+                invmat <<- NULL
+                det <<- NULL
+                rowcol <<- NULL
+        }
+        
+        getmat <- function() mat
+        
+        setrowcol <- function(temp) rowcol <<- temp
+        
+        getrowcol <- function() rowcol
+        
+        setdet <- function(temp) det <<- temp
+        
+        getdet <- function() det
+        
+        setinv <- function(inverse) invmat <<- inverse
+        
+        getinv <- function() invmat
+        
+        list(setrowcol = setrowcol, getrowcol = getrowcol ,setdet= setdet, getdet=getdet, setmat = setmat, getmat = getmat, setinv = setinv, getinv = getinv)
 } 
 
 ## cacheSolve() function does the following:
@@ -71,46 +71,46 @@ makeCacheMatrix <- function(mat = matrix()) {
 ## matrix inverse
 
 cacheSolve <- function(mat, ...) {
-  
-  detmat <- mat$getdet()
-  if (!is.null(detmat)) {
-    if (detmat == 0) {
-      message("Cached message: The matrix is non invertible since determinant is zero")
-      return(matrix())
-    }
-  }
-  
-  rowcolmat <- mat$getrowcol()
-  if (!is.null(rowcolmat)) {
-    if (rowcolmat == FALSE) {
-      message("Cached message: The matrix is non invertible since it is not a square matrix")
-      return(matrix())
-    }
-  }
-  
-  invmat <- mat$getinv()
-  if (!is.null(invmat)) {
-    message("Getting matrix inverse from cache")
-    return(invmat)
-  }
-  
-  matdata <- mat$getmat()
-  
-  if (nrow(matdata) != ncol(matdata)) {
-    message("The matrix is non invertible since it is not a square matrix")
-    mat$setrowcol(FALSE)
-    return(matrix())
-  }
-  mat$setrowcol(TRUE)
-  
-  detdata <- det(matdata)
-  mat$setdet(detdata)
-  if (detdata == 0) {
-    message("The matrix is non invertible since determinant is zero")
-    return(matrix())
-  }
-  
-  invmat <- solve(matdata, ...)
-  mat$setinv(invmat)
-  return(invmat)
+        
+        detmat <- mat$getdet()
+        if (!is.null(detmat)) {
+                if (detmat == 0) {
+                        message("Cached message: The matrix is non invertible since determinant is zero")
+                        return(matrix())
+                }
+        }
+        
+        rowcolmat <- mat$getrowcol()
+        if (!is.null(rowcolmat)) {
+                if (rowcolmat == FALSE) {
+                        message("Cached message: The matrix is non invertible since it is not a square matrix")
+                        return(matrix())
+                }
+        }
+        
+        invmat <- mat$getinv()
+        if (!is.null(invmat)) {
+                message("Getting matrix inverse from cache")
+                return(invmat)
+        }
+        
+        matdata <- mat$getmat()
+        
+        if (nrow(matdata) != ncol(matdata)) {
+                message("The matrix is non invertible since it is not a square matrix")
+                mat$setrowcol(FALSE)
+                return(matrix())
+        }
+        mat$setrowcol(TRUE)
+        
+        detdata <- det(matdata)
+        mat$setdet(detdata)
+        if (detdata == 0) {
+                message("The matrix is non invertible since determinant is zero")
+                return(matrix())
+        }
+        
+        invmat <- solve(matdata, ...)
+        mat$setinv(invmat)
+        return(invmat)
 }
